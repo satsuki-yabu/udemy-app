64ac34878559d6b1564a267678f5503137729fd3
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-19 10:58:51 +0900

    propsの説明

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 
-const Cat = () => {
-  return (
-    <>
-    <h1>にゃお</h1>
-    </>
-  )
-}
 
 const App = () => {
   return (
-        <>
-          <Cat/>
-          <Cat/>
-          <Cat/>
+    <>
+        {/* Userコンポーネントに対して、nameというProps（属性）を与えている */}
+          <User name={'Taro'} age={'10'}/>
+          <User name={'Hanako'} age={'5'}/>
         </>
       )
-}
+    }
+    
+    const User = (props) => {
+      return (
+        <>
+        <h1>Hi,I am{props.name}, and {props.age}years old</h1>
+        </>
+      )
+    }
 
-
-
-// function App() {
-//   return (
-//     <>
-//       <input type='text' onChange={()=>{console.log('I am clicked')}} />
-//     </>
-//   )
-  
-// }
 
 export default App;
