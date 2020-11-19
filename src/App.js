849f0b353fe0b8b2535c419fa38af20f8f49244e
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-19 16:28:52 +0900

    propsを学習した

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 
-
+// 親コンポーネント
 const App = () => {
+
+const profiles = [
+  // arrayの中にハッシュを入れます。ハッシュ→:の前がkey,:の後ろがvalue。keyに基づいたvalueを受け取ることができる。
+  {name: 'Taro', age: '10'},
+  {name: 'Hanako', age: '5'},
+  {name: 'Noname'}
+]
   return (
     <>
-        {/* Userコンポーネントに対して、nameというProps（属性）を与えている */}
-          <User name={'Taro'} age={'10'}/>
-          <User name={'Hanako'} age={'5'}/>
+      {/* map関数は一つのハッシュを受け取りますmap()→()の中にハッシュを書く。（）はアロー関数で、上記で設定したハッシュを回す */}
+      {/* 関数はJSXの記法で書く→{}で囲わないといけない */}
+      {/* Reactには仮想DOMがあり、どのDOMが変更になったのかを管理しています */}
+      {/* それぞれのDOMにkeyを与えて、必要最低限のDOMの範囲を管理しています、DOMを指し示す。keyが必要になってくる。mapで受け取るindexを利用して、keyを与えてあげる */}
+      {profiles.map((profile,index) => {
+        return (
+          <User name={profile.name} age={profile.age} key={index}/>
+        )
+      })}
         </>
       )
     }
-    
+
+    // 子コンポーネント
+
     const User = (props) => {
       return (
         <>
@@ -17,6 +32,10 @@ const App = () => {
         </>
       )
     }
-
-
+  
+  User.defaultProps = {
+    age: 1
+  }
+  
+    
 export default App;
