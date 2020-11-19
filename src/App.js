04ad47d49a31391ff76c75936de67f4f7b455a99
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-19 19:21:03 +0900

    stateの状態管理を学習した

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import PropTypes from 'prop-types'
-// 親コンポーネント
-const App = () => {
+import React, {Component} from 'react';
 
-const profiles = [
-  // arrayの中にハッシュを入れます。ハッシュ→:の前がkey,:の後ろがvalue。keyに基づいたvalueを受け取ることができる。
-  {name: 'Taro', age: 10},
-  {name: 'Hanako', age: 5},
-  {name: 'NoName', age: 6}
-]
-  return (
-    <>
-      {/* map関数は一つのハッシュを受け取りますmap()→()の中にハッシュを書く。（）はアロー関数で、上記で設定したハッシュを回す */}
-      {/* 関数はJSXの記法で書く→{}で囲わないといけない */}
-      {/* Reactには仮想DOMがあり、どのDOMが変更になったのかを管理しています */}
-      {/* それぞれのDOMにkeyを与えて、必要最低限のDOMの範囲を管理しています、DOMを指し示す。keyが必要になってくる。mapで受け取るindexを利用して、keyを与えてあげる */}
-      {profiles.map((profile,index) => {
+// propsは変更不可能な値（イミュータブル）親から子へ渡すことができる
+// stateは変更可能な値（ミュータブル）コンポーネントの内部でのみ使用される
+
+// AppがCounterコンポーネントを呼びます
+const App = () => (<Counter></Counter>)
+// このCounterコンポーネントの中で、Stateを使っていきたいので、classコンポーネントにします
+// →これまではstateは、classコンポーネントの中でしか使用できなかった。しかし、React16.8で導入されたHookで、関数コンポーネントの中でも使えるようになった
+
+class Counter extends Component {
+  // stateに状態をもたせてみましょう
+  // コンポーネントの初期化には、constructorを使用します。
+    constructor(props) {
+      // 親クラスで初期化
+      super(props)
+      this.state = { count: 0 }
+    }
+
+    handlePlusButton = () => {
+      // 状態を変化させる（+1など）
+      this.setState({ count: this.state.count +1 })
+    }
+
+    handleMinusButton = () => {
+      this.setState({count: this.state.count -1})
+    }
+      render() {
         return (
-          <User name={profile.name} age={profile.age} key={index}/>
-        )
-      })}
-        </>
-      )
-    }
-
-    // 子コンポーネント
-
-    const User = (props) => {
-      return (
         <>
-        <h1>Hi,I am{props.name}, and {props.age}years old</h1>
+        <div>counter: {this.state.count}</div>
+        {/* まずは属性を書きます →onClick属性*/}
+        {/* このクラスのインスタンスで、メソッドを呼び出し、そのメソッドの中でstateの値を変更する */}
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
         </>
-      )
-    }
+        )
+     }
+  }
 
-    // Userコンポーネントに対するpropTypesをここで定義する
-    // isRequiredは「必ず存在しないとだめですよ」というもの
-    User.propTypes = {
-      name: PropTypes.string,
-      age: PropTypes.number.isRequired
-    }
-  
-    
 export default App;
+
+// 【？】なぜsuperメソッドを指定しなければならないのか？？
+// →該当のclassが、何か別のclassに依存しているから。今回は、既存のComponentクラスを継承している。
+// →継承は便利な反面、初期化を行わなければならない。
+// ただし、constructor内の記述が、super(props)だけということであれば、省略できる場合もある
+
+// アロー関数で{}と()の違い
+
+
+// メソッドは、定数定義const~は必要ない
+
+// 【？】handlePlusButtonは関数なのに、なぜ引数()が必要ないのか？
+// →onClickによるコールバック関数には()ありなしの方法がある
