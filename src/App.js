40eb84ed406409cb807ac96b09b5dd3c781b29c1
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-19 09:58:11 +0900

    JSXでこんな書き方もできるよ１

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 
 function App() {
-  return (
-      <h1>Hello,World!!</h1>
-  );
+  // h1タグの部分が、domという変数に代入される。この時もJSXが機能している。
+  const dom = <h1>Hello,World!!</h1>;
+  return dom;
 }
 
 export default App;
