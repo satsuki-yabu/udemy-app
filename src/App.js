b70915bc934d782327469b01cb0966c3ed4eb87f
satsuki-yabu <tetecoo.1019@gmail.com>
2020-11-19 10:01:24 +0900

    jsxでこんな書き方もできるよ２

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
 
 function App() {
-  // h1タグの部分が、domという変数に代入される。この時もJSXが機能している。
-  const dom = <h1>Hello,World!!</h1>;
+  // JSXは{}でjavascriptが記述できる。
+  const greeting = 'Hi'
+  const dom = <h1>{greeting}</h1>;
   return dom;
 }
 
